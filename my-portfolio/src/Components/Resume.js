4bacfe6550c4b825a513db23eb600322b7520d29
Abhikesh <abhikeshwale@gmail.com>
2023-07-11 22:50:13 +0900

    EOD update 11/7

--- my-portfolio/src/Components/Resume.js
+++ my-portfolio/src/Components/Resume.js
@@ -8,73 +8,94 @@ const Resume = () => {
         <p className="absolute mt-6 mb-6 text-4xl ">Resume</p>
         <span className="border absolute mt-10    border-teal-500 w-20"></span>
       </div>
-      <div className="flex justify-center max-sm:flex-col max-sm:justify-center" id="summary-container">
-        <div id="education-container" className="m-4 w-1/2 max-w-xl max-sm:w-auto">
+      <div
+        className="flex justify-center max-sm:flex-col max-sm:justify-center"
+        id="summary-container">
+        <div
+          id="education-container"
+          className="ml-8 mr-4 w-1/2 max-w-xl max-sm:w-auto">
           left
           <h1>My Education</h1>
           <div
             id="education-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5">
+            className="border border-gray-400 rounded-lg p-6 mb-8">
             <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
               2014 - 2018
             </p>
             <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
             <p>Savitribai Phule Pune University</p>
-
           </div>
           <div
             id="education-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5">
+            className="border border-gray-400 rounded-lg p-6 mb-8">
             <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
               2014 - 2018
             </p>
             <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
             <p>Savitribai Phule Pune University</p>
-
           </div>
           <div
             id="education-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5">
+            className="border border-gray-400 rounded-lg p-6 mb-8">
             <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
               2014 - 2018
             </p>
             <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
             <p>Savitribai Phule Pune University</p>
-
           </div>
-        </div>"
-        <div id="experiance-container" className="m-4 w-1/2 max-w-xl max-sm:w-auto">
+        </div>
+        "
+        <div
+          id="experiance-container"
+          className="ml-4 mr-8 mb-4 w-1/2 max-w-xl max-sm:w-auto">
           right
           <h1>My Experiance</h1>
           <div
             id="experiance-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5">
-            <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">                               
-              2014 - 2018                                                       
-            </p>
-            <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
-            <p>Savitribai Phule Pune University</p>
-
-          </div>
-          <div
-            id="experiance-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5">
+            className="border border-gray-400 rounded-lg p-6 mb-8">
             <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
               2014 - 2018
             </p>
             <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
             <p>Savitribai Phule Pune University</p>
-
           </div>
           <div
             id="experiance-card"
-            className="border border-gray-400 rounded-lg p-6 mb-5                                                                                                           ">
+            className="border border-gray-400 rounded-lg p-6 mb-8">
             <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
               2014 - 2018
             </p>
             <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
             <p>Savitribai Phule Pune University</p>
-
+          </div>
+          <div
+            id="experiance-card"
+            className="border border-gray-400 rounded-lg p-6 mb-8                                                                                                           ">
+            <p className="text-center bg-cyan-800 rounded-lg inline-block px-4 ">
+              2014 - 2018
+            </p>
+            <h1 className="text-2xl py-2">Bachelor of Enginerring</h1>
+            <p>Savitribai Phule Pune University</p>
+          </div>
+        </div>
+      </div>
+      <div
+        id="skills-container"
+        className="flex justify-center max-sm:flex-col max-sm:justify-center">
+        <h1>Skill</h1>
+        <div className="flex ">
+          <div>
+            <div className="ml-8 mr-4 w-1/2 max-w-xl max-sm:w-auto"> 
+              <div className=" relative flex justify-between">
+                <p className="justify-end">HTML</p>
+                <p>50%</p>
+              </div>
+              <div>
+                <span className="border absolute  h-[10px] pb-[10px] rounded-lg border-teal-500 w-1/2 max-w-xl">
+                  <span className="border  absolute justify-center h-[10px]  bg-slate-800 rounded-r-none rounded-l-lg border-teal-500 w-1/2"></span>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
