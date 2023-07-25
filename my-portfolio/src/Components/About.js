2aaf839e16903c9705217480aabf37d4eaa5438c
Abhikesh <abhikeshwale@gmail.com>
2023-07-26 07:18:45 +0900

    added Responsiveness for About page

--- my-portfolio/src/Components/About.js
+++ my-portfolio/src/Components/About.js
@@ -8,25 +8,23 @@ const About = () => {
       className="py-32 flex flex-col bg-slate-50 justify-center items-center">
       <TitleCard title="ABOUT ME" subTitle="Know Me More" />
       <div className="flex justify-between pt-7 max-sm:flex-col">
-        <div className="flex-col pt-4  " id="about-container">
-          <h2 className="text-3xl font-bold leading-10">
+        <div className="flex-col pt-4 " id="about-container">
+          <h2 className="text-3xl font-bold leading-10  pt-4  ">
             I'm <span className="text-emerald-400">Abhikesh Wale,</span> a
             FrontEnd Developer
           </h2>
-          <p className="text-base leading-loose">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incis nostrud exerlaborum.
+          <p className="text-base  pt-4 ">
+          I am having 4.5+ years of experience in maintaining and building responsive web applications.
           </p>
-          <p className="text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididuitation ullamco laboris nin reprehenderit in
-            voluptate velip proidentollorum.
+          <p className="text-base  pt-4 ">
+          Having good knowledge in HTML, CSS, JavaScript and Modern Libraries and Frameworks like ReactJS. Passionate
+about Web development along with JLPT N2 Japanese proficiency
           </p>
         </div>
         <div
-          className="flex flex-1 flex-col w-1/3 max-sm:justify-center max-sm:pr-[25%]  max-sm:pl-[15%] max-sm:w-1/2 "
+          className="flex flex-col"
           id="personal-info-container">
-            <div> <table className=" ">
+            <div className="flex flex-1 flex-col  max-sm:justify-center "> <table className=" ">
             <tr className="border-b ">
               <td className="p-4">
                 <b className="text-emerald-400">Name :</b> Abhikesh Wale
@@ -49,12 +47,12 @@ const About = () => {
               </td>
             </tr>
           </table></div>
-         <div className="flex justify-center">
-        <a href={resume} download="Abhikesh Resume">  <button className="text-white border m-1 p-2 h-[45px] w-44 duration-500 rounded-full px-3 bg-emerald-400 hover:bg-emerald-500 hover:shadow-[0 5px 15px rgba(0,0,0,.15)] ">Download CV</button>
+         <div className="flex justify-center mt-4 max-sm:px-[50%]  ">
+        <a href={resume} download="Abhikesh Resume">  <button className="text-white border h-[45px] w-44 duration-500 rounded-full px-3 bg-emerald-400 hover:bg-emerald-500 hover:shadow-[0 5px 15px rgba(0,0,0,.15)] ">Download CV</button>
         </a>  </div>  </div>
       </div>
-      <div id="about-summury-container" className="pt-4">
-        <div className="flex justify-between gap-9">
+      <div id="about-summury-container" className="pt-8">
+        <div className="flex justify-between gap-9 max-sm:flex-col">
           <div className="flex flex-col items-center">
             <h1 className="text-4xl font-bold "> 4.5+ </h1>
             <h1> Years Experiance </h1>
